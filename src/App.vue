10545ceeb7d70c4985496b3a6f6dc557bba71710
Владислав Антонов <lolik1806@gmail.com>
2022-09-22 08:09:36 +0300

    init

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="nav">
+        <nav>
+            NAV
+        </nav>
+    </div>
+    <div class="body">
+        <RouterView/>
+    </div>
+</template>
+
+<script setup>
+import {RouterLink, RouterView} from 'vue-router';
+
+</script>
+
+<style scoped>
+.nav {
+    background-color: var(--color-background-soft);
+    border-color: var(--color-border);
+    height: auto;
+    margin: 10px;
+}
+
+.body {
+    background-color: aqua;
+    height: auto;
+    flex-grow: 4;
+    margin: 10px;
+}
+</style>
