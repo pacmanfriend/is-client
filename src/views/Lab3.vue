004c021a374f9d78019f1904e123716231e1b05e
Vladislav Antonov <lolik1806@gmail.com>
2022-10-31 20:54:59 +0300

    lab 3

--- src/views/Lab3.vue
+++ src/views/Lab3.vue
@@ -8,6 +8,22 @@
         <Textarea v-model="text" :disabled="true" :autoResize="true" rows="10" cols="50"/>
     </div>
     <div>
+        <h3>Символы с кодировкой</h3>
+        <DataTable class="p-datatable-sm" :value="chars" responsive-layout="scroll" :paginator="true" :rows="10"
+                   currentPageReportTemplate="Showing {first} to {last} of {totalRecords}">
+            <Column headerStyle="width: 100px" v-for="col of columns" :field="col.field" :header="col.header"
+                    :key="col.field"></Column>
+        </DataTable>
+    </div>
+    <div>
+        <h3>Частотный анализ</h3>
+        <DataTable class="p-datatable-sm" :value="segments" responsive-layout="scroll" :paginator="true" :rows="10"
+                   currentPageReportTemplate="Showing {first} to {last} of {totalRecords}">
+            <Column header-style="width: 100px" v-for="col of frequencyTableColumns" :field="col.field"
+                    :header="col.header" :key="col.field"/>
+        </DataTable>
+    </div>
+    <div>
         {{ result }}
     </div>
     <div>
@@ -25,12 +41,29 @@ export default {
         return {
             selectedFile: null,
             files: [],
+            chars: null,
+            segments: null,
+            columns: null,
+            frequencyTableColumns: null,
             result: 0,
             bites: 0,
             text: ""
         }
     },
     created() {
+        this.columns = [
+            {field: 'symbol', header: 'Символ'},
+            {field: 'code', header: 'Кодировка'},
+        ]
+
+        this.frequencyTableColumns = [
+            {field: 'character', header: 'Символ'},
+            {field: 'frequency', header: 'Частота'},
+            {field: 'probability', header: 'Вероятность'},
+            {field: 'interval', header: 'Интервал'}
+        ]
+
+
         GetFiles().then(val => {
             if (val.error == null) {
                 this.files = val.files;
@@ -44,6 +77,8 @@ export default {
                 this.result = res.result;
                 this.text = res.text;
                 this.bites = res.bites;
+                this.chars = res.chars;
+                this.segments = res.segments;
             });
         }
     }
