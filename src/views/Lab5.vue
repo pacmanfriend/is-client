bab25d1ee82f5c44a7c5175567a662eea59ab0cf
Vladislav Antonov <lolik1806@gmail.com>
2022-11-08 08:21:46 +0300

    lab 5

--- src/views/Lab5.vue
+++ src/views/Lab5.vue
@@ -1,25 +1,31 @@
 <template>
     <h1>LAB 5</h1>
     <div>
+        <SelectButton v-model="selectedOption" :options="options" optionLabel="label" @change="changeSelectOptions"
+                      aria-labelledby="single"/>
         <Dropdown placeholder="Выберите файл" v-model="selectedFile" :options="files" option-label="FileName"
                   option-value="FileName" @change="selectFile"/>
     </div>
     <div>
         <Textarea v-model="text" :disabled="true" :autoResize="true" rows="10" cols="50"/>
+        <Textarea v-model="result_text" :disabled="true" :autoResize="true" rows="10" cols="50"/>
     </div>
 </template>
 
 <script>
 import {GetFiles} from "@/api/common-api";
-import {GammaEncryption} from "@/api/lab4-api";
+import {GetBlockEncrypt} from "@/api/lab5-api";
 
 export default {
     name: "Lab5.vue",
     data() {
         return {
             text: "",
+            result_text: "",
             selectedFile: "",
-            files: []
+            files: [],
+            selectedOption: null,
+            options: [],
         }
     },
     created() {
@@ -28,15 +34,24 @@ export default {
                 this.files = val.files;
             }
         });
+
+        this.options = [
+            {option: true, label: "Закодировать текст"},
+            {option: false, label: "Раскодировать текст текст"}
+        ]
     },
     methods: {
         selectFile() {
-            GammaEncryption(this.selectedFile, this.selectedOption.option).then(val => {
-                let res = val.data;
+            GetBlockEncrypt(this.selectedFile, this.selectedOption.option).then(value => {
+                let res = value.data;
 
                 this.text = res.text;
+                this.result_text = res.result;
             });
         },
+        changeSelectOptions(event) {
+            this.selectedOption = event.value;
+        }
     }
 }
 </script>
